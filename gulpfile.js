258379414d93e83878f13e2683ba5f3754cd304a
vladimirFr89 <silversssnake89@gmail.com>
2017-11-08 13:52:17 +0500

    Добавлены зависимости на pug, stylus и browser-sync. В gulpfile добавлены необходимые таски для сборки и обновления после внесения изменений в соответсвующий файл. Добавлены файлы для написания pug и stylus кода

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,38 @@
 var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var pug = require('gulp-pug');
+var browserSync = require('browser-sync');
 
 gulp.task('default', function() {
     // place code for your default task here
     console.log("Running!!!!!");
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    });
+});
+
+gulp.task('views', function buildHTML() {
+    return gulp.src('app/views/*.pug')
+        .pipe(pug({
+            /*pretty: true*/
+        }))
+        .pipe(gulp.dest('app'));
+});
+
+gulp.task('stylus', function () {
+    return gulp.src('app/stylus/**/*.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch',['browser-sync', 'views','stylus'], function() {
+    gulp.watch('app/views/*.pug', ['views'])
+    gulp.watch('app/stylus/**/*.styl', ['stylus']);
 });
